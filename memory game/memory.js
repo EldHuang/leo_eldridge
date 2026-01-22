1772e6e43cfeafc726db58c63f85a258a5f31c3c
EldHuang <eldridge.huang12@gmail.com>
2026-01-21 21:26:33 -0800

    Final Save 1/21/2026

--- memory game/memory.js
+++ memory game/memory.js
@@ -15,36 +15,14 @@ class memoryGame {
         this.patternBtn.addEventListener('click', () => this.newRound(5));
     }
 
+    generatePattern(len) {
+        return Array.from({ length: len }, () => Math.floor(Math.random() * 3))
+    }
+
     newRound(len) {
         this.pattern = this.generatePattern(len);
         console.log('new pattern created', this.pattern);
-        for (let i in this.pattern) {
-            setTimeout(() => this.showPattern(this.pattern[i]), 1000 * i);
-        }
-    }
 
-    generatePattern(len) {
-        return Array.from({ length: len }, () => Math.floor(Math.random() * 4))
-    }
-
-    showPattern(num) {
-        const currentOption = this.options[num];
-
-        currentOption.classList.add('active');
-
-        setTimeout(() => currentOption.classList.remove('active'), 500);
-    }
-
-    decideColor(opt) {
-        if (opt === 0) {
-            return 'green';
-        } else if (opt === 1) {
-            return 'red';
-        } else if (opt === 2) {
-            return 'yellow';
-        } else {
-            return 'blue';
-        }
     }
 }
 
